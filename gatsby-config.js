9c215af52dd73556eb07f4756e1fb1f2998c837a
Yoandry Collazo <ycbarrosocu@gmail.com>
2021-01-09 17:21:53 -0500

    👌 IMPROVE: Add Env variables & improve Metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,20 @@
+
+const envPath =
+process.env.NODE_ENV === 'development'
+  ? '.env.development'
+  : '.env.production';
+require('dotenv').config({
+path: envPath,
+});
+
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `Contact List`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
   },
   plugins: [
+    `gatsby-plugin-sass`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
